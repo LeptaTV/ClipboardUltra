51fa6fdb565bac619a00ee16cf0a25fdb2a05aeb
Alexandre Bensebane <gully.yew2987@eagereverest.com>
2026-07-28 08:56:54 +1000

    Initial Commit

---
+++ ClipboardUltra/ClipboardUltraApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardUltraApp.swift
+//  ClipboardUltra
+//
+//  Created by A B on 28/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardUltraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
